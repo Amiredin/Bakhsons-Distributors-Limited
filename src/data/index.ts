1512d0c00c5e92d79d50ceb4358116ae99fa7bdb
Amiredin <amiredin@yahooo.com>
2023-11-20 15:42:41 +0300

    fourth commit

--- src/data/index.ts
+++ src/data/index.ts
@@ -18,8 +18,8 @@ import E6 from "../assets/E6.png";
 import C1 from "../assets/C1.png";
 import C2 from "../assets/C2.png";
 import C3 from "../assets/C3.png";
-import C4 from "../assets/C4.png";
-import C5 from "../assets/C5.png";
+import C4 from "../assets/c4.png";
+import C5 from "../assets/c5.png";
 import C6 from "../assets/C6.png";
 
 export const heroCarouselData = [
