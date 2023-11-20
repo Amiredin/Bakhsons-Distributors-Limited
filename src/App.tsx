60e9bf389981abfa77f56b5b60e190775a895b75
Amiredin <amiredin@yahooo.com>
2023-11-20 15:27:18 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Header from "./components/Header";
 import WhoWeAre from "./components/WhoWeAre";
 import VissionMissionValue from "./components/VissionMissionValue";
